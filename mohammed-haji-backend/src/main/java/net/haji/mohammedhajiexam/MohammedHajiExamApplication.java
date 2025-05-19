615b10f3a2ff89a6354c5e9ce17cff3ed401f77c
mohammedhaji <hmohammediaji@gmail.com>
2025-05-19 09:01:12 +0100

    add the backend and frontend folders

---
+++ mohammed-haji-backend/src/main/java/net/haji/mohammedhajiexam/MohammedHajiExamApplication.java
@@ -0,0 +1,13 @@
+package net.haji.mohammedhajiexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohammedHajiExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohammedHajiExamApplication.class, args);
+    }
+
+}
---
+++ mohammed-haji-backend/src/test/java/net/haji/mohammedhajiexam/MohammedHajiExamApplicationTests.java
@@ -0,0 +1,13 @@
+package net.haji.mohammedhajiexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohammedHajiExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
